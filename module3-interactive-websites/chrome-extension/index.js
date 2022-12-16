7ff81560b213822e6ac241c02d68a2c6b21251b4
Peter Huynh <peterlonghaihuynh25@gmail.com>
2022-12-16 17:15:42 -0500

    update

--- module3-interactive-websites/chrome-extension/index.js
+++ module3-interactive-websites/chrome-extension/index.js
@@ -2,8 +2,12 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const delBtn = document.getElementById("del-btn")
+const tabBtn = document.getElementById("tab-btn")
 
-localStorage.setItem()
+// JSON get the value from key (which is the array name)
+// and turn them back into string
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 // Instead of using onClick on the element in the HTML body
 // Use addEventListener in js file for leaner code
@@ -12,24 +16,54 @@ inputBtn.addEventListener("click", function () {
     if (inputEl.value.length != 0) {
         myLeads.push(inputEl.value)
         inputEl.value = ""
+        // local storage only store strings so JSON will turn array in to string
+        // and store it in local storage
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
     }
-    renderLeads()
+    render(myLeads)
 })
 
-const renderLeads = () => {
+tabBtn.addEventListener("click", function () {
+    // Grab URL of the current tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        // tabs are in an array so we need to select current tab as tabs[0]
+        // tabs[0] is object, to get url, we need to do tabs[0].url
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+delBtn.addEventListener("dblclick", function () {
+    // clear the local storage
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+const render = (leads) => {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         // innerHTML will recognize the HTML syntax and render elements
         // properly
         listItems += `
-        <li>
-            <a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a>
-        </li>`
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+            `
         // Alternative ways
         // const li = document.createElement("li")
-        // li.textContent = myLeads[i]
+        // li.textContent = leads[i]
         // ulEl.append(li)
     }
 
     ulEl.innerHTML = listItems
+}
+
+// Assign leads from storage to myLeads
+if (leadsFromLocalStorage != null) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
