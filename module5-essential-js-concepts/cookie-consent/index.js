b986cab4b68ec8ed002a75f6a330dce1f0b5c8a2
Peter Huynh <peterlonghaihuynh25@gmail.com>
2022-12-29 12:48:32 -0500

    update

---
+++ module5-essential-js-concepts/cookie-consent/index.js
@@ -0,0 +1,23 @@
+const modal = document.getElementById("modal")
+const modalCloseBtn = document.getElementById("modal-close-btn")
+const consentForm = document.getElementById("consent-form")
+const modalText = document.getElementById("modal-text")
+
+setTimeout(function () {
+    modal.style.display = "inline"
+}, 3000)
+
+modalCloseBtn.addEventListener("click", function () {
+    modal.style.display = "none"
+})
+
+consentForm.addEventListener("submit", function (e) {
+    e.preventDefault()
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>`
+})
