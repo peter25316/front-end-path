9a9d8b96015d23a0eae6b8c42fd0b47e87f8eda6
Peter Huynh <peterlonghaihuynh25@gmail.com>
2023-01-04 23:01:19 -0500

    update

--- module5-essential-js-concepts/cookie-consent/index.js
+++ module5-essential-js-concepts/cookie-consent/index.js
@@ -2,6 +2,7 @@ const modal = document.getElementById("modal")
 const modalCloseBtn = document.getElementById("modal-close-btn")
 const consentForm = document.getElementById("consent-form")
 const modalText = document.getElementById("modal-text")
+const declineBtn = document.getElementById("decline-btn")
 
 setTimeout(function () {
     modal.style.display = "inline"
@@ -11,8 +12,17 @@ modalCloseBtn.addEventListener("click", function () {
     modal.style.display = "none"
 })
 
+declineBtn.addEventListener("mouseenter", function () {
+    document.getElementById("modal-choice-btns").classList.toggle("reverse")
+})
+
 consentForm.addEventListener("submit", function (e) {
     e.preventDefault()
+    // Store the data from consentForm element into a js object type FormData 
+    const consentFormData = new FormData(consentForm)
+
+    const formName = consentFormData.get("fullName")
+
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
@@ -20,4 +30,18 @@ consentForm.addEventListener("submit", function (e) {
             Uploading your data to the dark web...
         </p>
     </div>`
+
+    setTimeout(function () {
+        document.getElementById("uploadText").textContent = "Making the sale..."
+    }, 1500)
+
+    setTimeout(function () {
+        document.getElementById("modal-inner").innerHTML = `
+        <h2>Thanks <span class="modal-display-name">${formName}</span>, you sucker! </h2>
+        <p>We just sold the rights to your eternal soul.</p>
+        <div class="idiot-gif">
+            <img src="images/pirate.gif">
+        </div>`
+        modalCloseBtn.disabled = false
+    }, 3000)
 })
