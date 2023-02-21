6e54be6751d2776a060d78676ece5b9d586cbfff
Peter Huynh <70732959+peter25316@users.noreply.github.com>
2023-02-20 20:11:29 -0500

    update

---
+++ module12-advanced-react/my-app/src/pages/components/work1.jsx
@@ -0,0 +1,9 @@
+const Work1 = () => {
+  return (
+    <div>
+      <p>This is Work1</p>
+    </div>
+  );
+};
+
+export default Work1;
--- module12-advanced-react/my-app/src/pages/components/works.jsx
+++ module12-advanced-react/my-app/src/pages/components/works.jsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 const Works = () => {
   return (
     <div>
-      <h2>This is Works</h2>
+      <h2>This is Works List :</h2>
+      <Link href="/components/work1">Work 1</Link>
     </div>
   );
 };
